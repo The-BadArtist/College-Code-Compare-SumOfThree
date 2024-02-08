a5af2f8b6c35cd3388be2e848e48f569057b474e
The-BadArtist <javianhuell@gmail.com>
2024-02-08 16:38:36 -0500

    [UPDATE] Added a python name main

--- SumOfThree.py
+++ SumOfThree.py
@@ -1,7 +1,9 @@
 '''
 A simple program to add three numbers together.
 '''
+def sum (x, y, z):
+    sum = (x + y + z)
+    print(x, " + ", y, " + ", z, " = ", sum)
 
-x , y, z = 1, 2, 3
-sum = (x + y + z)
-print(x, " + ", y, " + ", z, " = ", sum)
+if __name__ == "__main__":
+    sum(1, 2, 3)
