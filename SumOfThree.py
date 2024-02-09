ebc1db20c19db3f115cf8f3c23a12653905317c7
The-BadArtist <javianhuell@gmail.com>
2024-02-08 23:31:00 -0500

    [ADD] Writability, Reliability, Readability

--- SumOfThree.py
+++ SumOfThree.py
@@ -1,9 +1,26 @@
 '''
 A simple program to add three numbers together.
 '''
-def sum (x, y, z):
-    sum = (x + y + z)
-    print(x, " + ", y, " + ", z, " = ", sum)
+def sum (number1 = 0, number2 = 0, number3 = 0, *args):
+# def sum (arg = 0, *args):
+    if len(args) > 0: 
+        #* Checks if there is any addition variables
+        print("Only 3 numbers try again")
+    else:
+        #Function to add the three parameters
+        sum = (number1 + number2 + number3)
+        #Prints the output in a regular addition format 
+        print(number1, " + ", number2, " + ", number3, " = ", sum) 
 
+    
+    # sum = arg
+    # if len(args) > 0 :
+    #     for number in args:
+    #         sum += number
+
+    # print(sum)
+
+    
 if __name__ == "__main__":
-    sum(1, 2, 3)
+    sum(1, 2, 3) # inputs sum(number1 , number2, number3)
+    
